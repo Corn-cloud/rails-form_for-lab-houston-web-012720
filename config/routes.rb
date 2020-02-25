ea5ff20b8cbda462306709e8788d72e30c428427
corn-cloud <comowaiye@gmail.com>
2020-02-25 15:54:00 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,29 @@
 Rails.application.routes.draw do
+  # get 'students/index'
+
+  # get 'students/show'
+
+  # get 'students/new'
+
+  # get 'students/create'
+
+  # get 'students/edit'
+
+  # get 'students/update'
+
+  # get 'school_classes/index'
+
+  # get 'school_classes/show'
+
+  # get 'school_classes/new'
+
+  # get 'school_classes/create'
+
+  # get 'school_classes/edit'
+
+  # get 'school_classes/update'
+  resources :students, only: [:show, :new, :create, :edit, :update]
+  resources :school_classes, only: [:show, :new, :create, :edit, :update]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+s1 = Student.create(first_name: 'Dami', last_name: 'soy')
+s2 = Student.create(first_name: 'Lola', last_name: 'Segz')
+s3 = Student.create(first_name: 'Phil', last_name: 'jones')
+
+c1 = SchoolClass.create('Math', 20)
+c1 = SchoolClass.create('English', 22)
+c1 = SchoolClass.create('Tech', 300)
+c1 = SchoolClass.create('Drama', 100)
